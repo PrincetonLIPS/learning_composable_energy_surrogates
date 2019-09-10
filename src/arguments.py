55e20f4a56076fd1983bd4e6176556b6654d577f
upbeat <alexub@gmail.com>
2019-09-10 17:20:01 -0400

    ramp down amp scale a bit

--- src/arguments.py
+++ src/arguments.py
@@ -78,7 +78,7 @@ parser.add_argument(
     "--boundary_amp_scale",
     type=float,
     help="maximum amplitude scale for boundary random fourier fn,",
-    default=1.0,
+    default=0.5,
 )
 parser.add_argument(
     "--force_freq_scale",
