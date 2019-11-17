b41671d5a72ed2c7e3ed4bf16a4233eb3935f1e0
Alex Beatson <alexub@gmail.com>
2019-11-17 15:07:07 -0500

    fix brackets

--- src/runners/trainer.py
+++ src/runners/trainer.py
@@ -311,7 +311,7 @@ class Trainer(object):
         ''
 
         if self.args.log_loss_scale:
-            f_loss = torch.nn.functional.mse_loss(torch.log(f+1e-7), torch.log(fhat+1e-7)
+            f_loss = torch.nn.functional.mse_loss(torch.log(f+1e-7), torch.log(fhat+1e-7))
             J_loss = torch.nn.functional.mse_loss(J / (self.train_J_std*loss_scale),
                                                   Jhat / (self.train_J_std*loss_scale))
         elif self.args.quadratic_loss_scale:
