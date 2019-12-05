f0db2444b2171855a56d17c31d675db62a14800c
Alex Beatson <alexub@gmail.com>
2019-12-05 16:24:33 -0500

    setup script

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source ~/.bashrc
+source ~/anaconda3/etc/profile.d/conda.sh
+conda remove --name nm --all
+conda create -n nm python==3.6.3
+conda activate nm
+conda install -c conda-forge dolfin-adjoint==2019.1.0
+conda install -c conda-forge mshr
+conda install pytorch torchvision -c pytorch
+conda install -c conda-forge hdf5==1.10.4
+conda install -c conda-forge numpy scipy matplotlib jupyter # pip install numpy scipy matplotlib jupyter
+pip install --upgrade ray==0.7.4
+pip install --upgrade tensorflow
