14fe3655da2840efe832a78ce7dc489e3d546e91
Alex Beatson <alexub@gmail.com>
2020-01-23 18:45:23 -0500

    make deploy collector step through solution trajectory

--- src/runners/deploy_collector.py
+++ src/runners/deploy_collector.py
@@ -7,8 +7,9 @@ from ..energy_model.surrogate_energy_model import SurrogateEnergyModel
 from ..data.sample_params import make_p, make_compression_deploy_bc
 from ..data.example import Example
 from ..nets.feed_forward_net import FeedForwardNet
-from src.energy_model.composed_energy_model import ComposedEnergyModel
-from src.energy_model.composed_fenics_energy_model import ComposedFenicsEnergyModel
+from ..energy_model.composed_energy_model import ComposedEnergyModel
+from ..energy_model.composed_fenics_energy_model import ComposedFenicsEnergyModel
+from ..util.solve import solve
 
 import random
 import numpy as np
@@ -35,21 +36,46 @@ class DeployCollectorBase(CollectorBase):
         params[:, 0] = args.c1
         params[:, 1] = args.c2
         force_data = torch.zeros_like(cem_boundary)
-        surr_soln = cem.solve(params, cem_boundary,
+        surr_soln, traj_u, traj_f, traj_g = cem.solve(params, cem_boundary,
                       constraint_mask, force_data,
-                      step_size=0.1, opt_steps=1000)
-        surr_cell_coords = torch.matmul(
-            cem.cell_maps.view(-1, cem.cell_maps.size(2)), surr_soln
-        ).view(cem.n_cells, -1, 2)
+                      step_size=0.1, opt_steps=1000,
+                      return_intermediate=True)
         cell_idx = np.random.choice(cem.n_cells)
 
-        self.bc = surr_cell_coords[cell_idx]
+        self.traj_u = [self.fsm.to_torch(torch.matmul(
+            cem.cell_maps.view(-1, cem.cell_maps.size(2)), u_i
+        ).view(cem.n_cells, -1, 2)[cell_idx]) for u_i in traj_u]
+
+        deltas = [
+            torch.norm(self.traj_u[i] - self.traj_u[i - 1]).data.cpu().numpy()
+            for i in range(1, len(self.traj_u))
+        ]
+
+        deltas = [0.0] + deltas
+        buckets = np.cumsum(deltas)
+        self.buckets = buckets / buckets[-1]
 
         self.stepsize = 1.0 / args.anneal_steps
         self.factor = 0.0
         self.steps = 0
         self.base_relax = self.args.relaxation_parameter
-        self.guess = fa.Function(self.fsm.V).vector()
+        self.guess = solve(self.fem, args,
+                           self.traj_u[0], fa.Function(self.fsm.V).vector(),
+                           torch.zeros_like(self.traj_u[0]), 50, 0.9)
+
+
+    def get_weighted_data(self, factor):
+        idx = np.searchsorted(self.buckets, factor) - 1
+        u1 = self.traj_u[idx]
+        u2 = self.traj_u[idx + 1]
+
+        residual = factor - self.buckets[idx]
+
+        alpha = residual / (self.buckets[idx + 1] - self.buckets[idx] + 1e-7)
+
+        u = alpha * u2 + (1.0 - alpha) * u1
+        return torch.Tensor(u.data)
+
 
 @ray.remote(resources={"WorkerFlags": 0.3})
 class DeployCollector(DeployCollectorBase):
