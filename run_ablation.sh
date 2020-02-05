fcd60479ee372768259f0856d77655230c97787b
Ubuntu <ubuntu@ip-172-31-41-237.us-east-2.compute.internal>
2020-02-05 06:54:06 +0000

    ablation on aws

--- run_ablation.sh
+++ run_ablation.sh
@@ -12,10 +12,12 @@ for ((i=0; i<n; i++)); do
     if [ ${switches[2]} -eq 1 ]; then remove_rigid='True'; else remove_rigid='False'; fi;	
     if [ ${switches[3]} -eq 1 ]; then Jw='1.0'; else Jw='0.'; fi;	
     if [ ${switches[4]} -eq 1 ]; then Hw='1.0'; else Hw='0.'; fi;	
-    command=$( IFS=$','; echo "python -m src.run --run_local True --lr 3e-4 --max_train_steps 10000 --data_name bV10_hmc_macro --experiment_name ablation_${switches[*]} --log_scale $logscale --quadratic_scale $quadratic_scale --remove_rigid $remove_rigid --J_weight $Jw --H_weight $Hw --batch_size 512 --train_size 55000 --val_size 5000 --ffn_layer_sizes [128,128,128] --max_collectors 0 --max_evaluators 0" )
+    command=$( IFS=$','; echo "python -m src.run --run_local True --lr 3e-4 --max_train_steps 10000 --results_dir /efs_nmor/results --data_name bV_10_hmc_macro_benchmark --experiment_name ablation_${switches[*]} --log_scale $logscale --quadratic_scale $quadratic_scale --remove_rigid $remove_rigid --J_weight $Jw --H_weight $Hw --batch_size 512 --train_size 55000 --val_size 5000 --ffn_layer_sizes [128,128,128] --max_collectors 0 --max_evaluators 0" )
     echo $command
-    cvd=$(( i%4 ))
-    # sleep 1
-    echo "CUDA_VISIBLE_DEVICES=$cvd $command &"
+    logfile=$"log_${switches[*]}.txt"
+    logfile=$(echo "${logfile// /}")
+    echo $logfile
+    $command &> $logfile &
+    sleep 30
     # sleep 1
 done;
